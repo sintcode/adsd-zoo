a0b81bd00e39c7dd847bbbc4d7251432937353e7
Jari Knoop <144428260+JariKN01@users.noreply.github.com>
2024-06-21 15:13:27 +0200

    Update Database.cs

--- Zoo/Models/Database.cs
+++ Zoo/Models/Database.cs
@@ -1,37 +1,19 @@
+using System.Collections.Generic;
+using System.Linq;
 using Zoo.Data;
 
 namespace Zoo.Models {
     public class Database {
-        public void AddAnimal(Animal animal) 
-        {
-            Species newSpecies = new()
-            {
-                Id = 1,
-                Name = "Lion",
-                LatinName = "Panthera Leo",
-                Size = Species.SizeClass.Large,
-                Diet = Species.DietType.Carnivore,
-                Activity = Species.ActivityPattern.Diurnal,
-                Predator = true,
-                SpaceRequired = 50.0,
-                SecurityRequired = Species.SecurityLevel.High
-            };
+        private List<Animal> animals = new List<Animal>();
 
-            Animal newAnimal = new() 
-            {
-                Name = "Alex",
-                SpeciesId = 1, //Using above-made 'Lion'
-                EnclosureId = 1, // assuming an enclosure with Id 1 exists
-            };
-            
-            Database db = new();
-            db.AddAnimal(newAnimal);
+        public void AddAnimal(Animal animal)
+        {
+            animals.Add(animal);
+            Console.WriteLine($"Added animal: {animal.Name}");
         }
 
         public Animal GetAnimal(int id) {
-            // Code to retrieve the animal from the database
-            // For now, return a dummy animal
-            return new Animal() { Id = id, Name = "Dummy" };
+            return animals.FirstOrDefault(a => a.Id == id);
         }
     }
 }
